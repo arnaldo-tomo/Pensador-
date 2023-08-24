957818500b4dfd0d5134ced9f205867330d8427c
Arnaldo tomo <arnaldotomo@gmail.com>
2023-08-24 10:34:00 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\ApiPensaodor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::get('/Categoria', [ApiPensaodor::class, 'categoria'])->name('categoria');
+Route::get('/frase', [ApiPensaodor::class, 'frase'])->name('frase');
