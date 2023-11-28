c62fea2f15359abaa6d98b90ae02ff24d643f5c7
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-28 11:15:48 +0200

    Update ApiPensaodor.php

--- app/Http/Controllers/ApiPensaodor.php
+++ app/Http/Controllers/ApiPensaodor.php
@@ -10,12 +10,20 @@ class ApiPensaodor extends Controller
 {
     public function categoria()
     {
-        $categorias = categoria::all();
+        $categorias = categoria::with('frases')->get();
         return  response()->json(compact('categorias'), 200);
     }
+
+    public function busca(Request $request, $nome)
+    {
+        $bsuca = categoria::where('nome', $nome)->first();
+        $allFrases = $bsuca->frases;
+        return response()->json(compact('allFrases'), 200);
+    }
+
     public function frase(Request $request)
     {
-        $frase = frase::all();
+        $frase = frase::with('categoria')->get();
         return  response()->json(compact('frase'), 200);
     }
 }
