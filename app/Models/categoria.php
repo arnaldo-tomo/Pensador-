7e528220ddb77c51bb1a531e8a8ee1de07f97510
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-28 11:16:04 +0200

    Update categoria.php

--- app/Models/categoria.php
+++ app/Models/categoria.php
@@ -6,14 +6,13 @@ use App\Models\frase;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class categoria extends Model
-{
+class categoria extends Model {
     use HasFactory;
     protected $table = 'categorias';
-    protected $fillable = ['nome'];
+    protected $fillable = [ 'nome' ];
 
-    public function frases(){
-        return $this->belongsToMany(frase::class, 'categoria_frases');
+    public function frases() {
+        return $this->belongsToMany( frase::class, 'categoria_frases' );
     }
 
 }
